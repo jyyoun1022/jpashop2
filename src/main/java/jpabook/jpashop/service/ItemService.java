3da18c0cf12032a5a28481b5134eb2a693623a8c
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2022-10-11 00:15:03 +0900

    상품 조회까지 완성

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,18 @@ public class ItemService {
     @Transactional
     public void save(Item item){
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, Book param){
+        // 더치체킹
+        // 트랜잭션 안에서 엔티티를 다시 조회, 변경할 값 선택
+        // -> 트랜잭션 커밋 시점에 변경감지하여 DB에 update 실행행
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
+
     }
 
     public Item findOne(Long id){
